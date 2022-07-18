a8f828be595d694926edc8d10e64cc9b893bfa03
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-07-18 10:53:12 +0200

    Fenêtre recherche Adhérent créée

--- src/main/java/fr/afpa/controllers/ControllerRechercherAdherent.java
+++ src/main/java/fr/afpa/controllers/ControllerRechercherAdherent.java
@@ -6,6 +6,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -15,19 +18,37 @@ import java.util.ResourceBundle;
 public class ControllerRechercherAdherent implements Initializable {
 
     @FXML
-    private Button btnRechercher;
+    private Button btnConsulterFicheAdherent;
+
+    @FXML
+    private Button btnMenuPrincipal;
+
+    @FXML
+    private Button btnRechercherAdherent;
+
+    @FXML
+    private Button btnValiderAdherent;
+
+    @FXML
+    private Label lblCotisationAJour;
+
+    @FXML
+    private Label lblDateFinCotisation;
+
+    @FXML
+    private Label lblNom;
+
+    @FXML
+    private Label lblPrenom;
+
+    @FXML
+    private Label lblRole;
+
+    @FXML
+    private TitledPane titledPaneAdherent;
 
     @FXML
     private TextField txtFIDAdherent;
-
-    @FXML
-    private Label lblTest;
-
-    @FXML
-    private Font x1;
-
-    @FXML
-    private Color x2;
 
     @FXML
     private Font x3;
@@ -36,17 +57,31 @@ public class ControllerRechercherAdherent implements Initializable {
     private Color x4;
 
     @FXML
-    void rechercher(ActionEvent event) {
-        rechercherAdherent();
+    void activerBoutons(final InputMethodEvent event) {
 
     }
 
-    private void rechercherAdherent(){
-        lblTest.setText(txtFIDAdherent.getText());
+    @FXML
+    void onClickMenuPrincipal(final MouseEvent event) {
+
+    }
+
+    @FXML
+    void rechercherAdherent(final ActionEvent event) {
+        //rechercherAdherent();
+
+    }
+
+    @FXML
+    void validerAdherent(final ActionEvent event) {
+
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(final URL location, final ResourceBundle resources) {
+        btnRechercherAdherent.setDisable(true);
+        btnConsulterFicheAdherent.setDisable(true);
+        btnValiderAdherent.setDisable(true);
 
     }
 }
