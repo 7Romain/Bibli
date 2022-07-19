af78125ac597f5b2470f503a5cffec0431892f2b
Jerome CHAPUT <106680913+Asperi137@users.noreply.github.com>
2022-07-19 09:59:40 +0200

    TODO remplir le tableau livre

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module fr.afpa.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens fr.afpa.app to javafx.fxml;
-    opens fr.afpa.controllers;
-    opens fr.afpa.entites;
-    exports fr.afpa.app;
-}
